c6cfe7026d1d8d5a4de5ace41a04de587daad4d7
jamalahmedmaaz <jamalahmedmaaz@gmail.com>
2014-08-08 19:44:55 +0530

    third

--- routes/segmentIO.js
+++ routes/segmentIO.js
@@ -1,5 +1,20 @@
-var context = require('rabbit.js').createContext('amqp://localhost');
-var pub = context.socket('PUBLISH');
+var amqp = require('amqp');
+var amqpconnection = amqp.createConnection({
+    host: 'turtle.rmq.cloudamqp.com',
+    login: 'jpophmne',
+    password: '8S64KCUj0fu5telpyNFdtfCLh0Nv1qHC',
+    connectionTimeout: 0,
+    vhost: 'jpophmne',
+    noDelay: true,
+    ssl: {
+        enabled: true
+    }
+}, function (err, data) {
+    if (err)
+        console.error('we failed ' + err);
+    else
+        console.log('passssed ' + data);
+});
 
 exports.post = function (request, response) {
     //    console.log(request);
@@ -23,8 +38,32 @@ function createPOJO(data) {
 function compress() {}
 
 function sendToQueue(segmentIOPayLoad) {
+
+
     console.log(JSON.stringify(segmentIOPayLoad));
-    pub.connect('SEGMENT_IO_SYNC');
-    console.log(pub.write(JSON.stringify(segmentIOPayLoad), 'utf8'));
+
+    //console.log(connection);
+
+    var exchange = amqpconnection.exchange('gainsight-click-stream-exchange', {
+        type: 'fanout',
+        durable: false
+    }, function () {
+        // Declaring a queue with a bogus name
+        var queue = amqpconnection.queue('gainsight-click-stream-queue', {
+            durable: false,
+            exclusive: true
+        }, function () {
+            console.log("q234");
+        });
+
+        // callback when queue binding was successful...
+        queue.on('queueBindOk', function () {
+            console.log("BIND OK....");
+        });
+
+        exchange.publish("message.text", "NodeJS says HOWDY! " + Date.now());
+    });
+
+    console.log("-------");
 
 }
