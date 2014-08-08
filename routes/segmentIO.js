2c4b35f330ce20a7523cb9201d96bd95f7a3e6c5
jamalahmedmaaz <jamalahmedmaaz@gmail.com>
2014-08-08 19:11:48 +0530

    Step 2

--- routes/segmentIO.js
+++ routes/segmentIO.js
@@ -1,10 +1,30 @@
 var context = require('rabbit.js').createContext('amqp://localhost');
 var pub = context.socket('PUBLISH');
 
-
 exports.post = function (request, response) {
-    console.log(request);
+    //    console.log(request);
     var bodyObject = request.body;
+    addToQueue(bodyObject);
     response.send(JSON.stringify(bodyObject));
 }
 
+function addToQueue(data) {
+    var payload = createPOJO(data);
+    sendToQueue(payload);
+}
+
+function createPOJO(data) {
+    var segmentIOPayLoad = {};
+    segmentIOPayLoad.data = data;
+    segmentIOPayLoad.subType = 'SEGMENT_IO';
+    return segmentIOPayLoad;
+}
+
+function compress() {}
+
+function sendToQueue(segmentIOPayLoad) {
+    console.log(JSON.stringify(segmentIOPayLoad));
+    pub.connect('SEGMENT_IO_SYNC');
+    console.log(pub.write(JSON.stringify(segmentIOPayLoad), 'utf8'));
+
+}
