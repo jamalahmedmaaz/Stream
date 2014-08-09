7b3debf620801b2fab19145fe7809a87ee01be7f
Ankit Jain <ankitjainist@gmail.com>
2014-08-09 09:44:42 +0530

    Adding caching for accessKey verification

--- routes/auth.js
+++ routes/auth.js
@@ -1,43 +1,43 @@
-//Required configuration 
-
 exports.verifyAccessToken = function (req, res, next) {
   var accessKey = req.header('accessKey');
-  if(!accessKey) {
-      res.status(403);
-      res.send('Unauthorized');
+  if (!accessKey) {
+    res.status(403);
+    res.send('Unauthorized');
+    return;
   }
-  
-    //gettting accesskey from Nodecache
+
+  // Get accesskey from Nodecache
   var nodecache = req.app.get('nodeCache');
   var cacheObj = nodecache.get(accessKey)[accessKey];
-    console.log(JSON.stringify(cacheObj));
-  if(!cacheObj) {
-      var mongo = req.app.get('mongoConnection');
-      if (!mongo) {
-        res.status(500);
-        res.send('Not yet intialized. Try after some time');
-      }
-      var collection = mongo.collection('accessKey');
-      collection.findOne({
-        'accessKeyHashed': accessKey
-      }, ['tenantId', 'projectId'], function (err, object) {
 
-        if (object && object.tenantId) {
-          //console.log("Found", object);
-          nodecache.set(accessKey, {isValid:true}); 
-          next();
-        } else {
-          nodecache.set(accessKey, {isValid:false}); 
-          res.status(403);
-          res.send('Unauthorized');
-        }
-      });
-  } else {
-      if(cacheObj.isValid) {
-          next();
+  if (!cacheObj) {
+    console.log('Cache miss. Hit the mongo');
+    var mongo = req.app.get('mongoConnection');
+    var collection = mongo.collection('accessKey');
+    collection.findOne({
+      'accessKeyHashed': accessKey
+    }, ['tenantId', 'projectId'], function (err, object) {
+
+      if (object && object.tenantId) {
+        //console.log("Found", object);
+        nodecache.set(accessKey, {
+          isValid: true
+        });
+        next();
       } else {
-          res.status(403);
-          res.send('Unauthorized');
+        nodecache.set(accessKey, {
+          isValid: false
+        });
+        res.status(403);
+        res.send('Unauthorized');
       }
+    });
+  } else {
+    if (cacheObj.isValid) {
+      next();
+    } else {
+      res.status(403);
+      res.send('Unauthorized');
+    }
   }
 };
