fd004445f0b47a07312bf5e367adca5f9dad5a00
jamalahmedmaaz <jamalahmedmaaz@gmail.com>
2014-08-10 23:48:23 +0530

    New Config

--- routes/segmentIO.js
+++ routes/segmentIO.js
@@ -8,54 +8,55 @@ var queue;
 
 // Open a connection
 var amqpconnection = amqp.createConnection({
-  host: 'turtle.rmq.cloudamqp.com',
-  login: 'jpophmne',
-  password: '8S64KCUj0fu5telpyNFdtfCLh0Nv1qHC',
-  connectionTimeout: 0,
-  vhost: 'jpophmne',
-  noDelay: true,
-  ssl: {
-    enabled: true
-  }
+    host: 'tiger.cloudamqp.com',
+    login: 'bufgnrqc',
+    password: 'BWMcX10ISxSvmOSRcMFcidw3WLWGFyXr',
+    connectionTimeout: 0,
+    vhost: 'bufgnrqc',
+    noDelay: true,
+    ssl: {
+        enabled: true
+    }
 }, {
-  defaultExchangeName: exchangeName,
-  reconnect: true, // Enable reconnection
-  reconnectBackoffStrategy: 'linear',
-  reconnectBackoffTime: 1000, // Try reconnect once a second
+    defaultExchangeName: exchangeName,
+    reconnect: true, // Enable reconnection
+    reconnectBackoffStrategy: 'linear',
+    reconnectBackoffTime: 1000, // Try reconnect once a second
 });
 
 // When connected create the exchange
 amqpconnection.on('ready', function () {
-  // declare the default exchange
-  exchange = amqpconnection.exchange(exchangeName);
+    // declare the default exchange
+    exchange = amqpconnection.exchange(exchangeName);
 });
 
 exports.post = function (request, response) {
-  addToQueue(request.body);
-  response.send(true);
+    addToQueue(request.body);
+    response.send(true);
 }
 
 function addToQueue(data) {
-  var payload = createPOJO(data);
-  sendToQueue(payload);
+    var payload = createPOJO(data);
+    sendToQueue(payload);
 }
 
 function createPOJO(data) {
-  var segmentIOPayLoad = {};
-  segmentIOPayLoad.data = data;
-  segmentIOPayLoad.subType = 'SEGMENT_IO';
-  return segmentIOPayLoad;
+    var segmentIOPayLoad = {};
+    segmentIOPayLoad.data = data;
+    segmentIOPayLoad.subType = 'SEGMENT_IO';
+    return segmentIOPayLoad;
 }
 
-function compress() {}
+function compress() {
+}
 
 function sendToQueue(segmentIOPayLoad) {
-  if (!exchange) {
-    console.error('Payload skipped. Exchange is not yet ready', segmentIOPayLoad);
-    return;
-  }
-  // publish a message
-  exchange.publish(queueName, {
-    body: segmentIOPayLoad
-  });
+    if (!exchange) {
+        console.error('Payload skipped. Exchange is not yet ready', segmentIOPayLoad);
+        return;
+    }
+    // publish a message
+    exchange.publish(queueName, {
+        body: segmentIOPayLoad
+    });
 }
